31dc43f44527815f13e3c36bfeb69c50a06aff41
Sergey Goppikov <grey2k@gmail.com>
2022-07-14 14:48:47 +0300

    Project Completed

--- Honest Calculator/task/honest_calc.py
+++ Honest Calculator/task/honest_calc.py
@@ -3,11 +3,23 @@ msg_0 = "Enter an equation"
 msg_1 = "Do you even know what numbers are? Stay focused!"
 msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
 msg_3 = "Yeah... division by zero. Smart move..."
+msg_4 = "Do you want to store the result? (y / n):"
+msg_5 = "Do you want to continue calculations? (y / n):"
+msg_6 = " ... lazy"
+msg_7 = " ... very lazy"
+msg_8 = " ... very, very lazy"
+msg_9 = "You are"
+msg_10 = "Are you sure? It is only one digit! (y / n)"
+msg_11 = "Don't be silly! It's just one number! Add to the memory? (y / n)"
+msg_12 = "Last chance! Do you really want to embarrass yourself? (y / n)"
 
 ADD = '+'
 SUB = '-'
 MUL = '*'
 DIV = '/'
+
+YES = 'y'
+NO = 'n'
 
 OPERATIONS = [
     ADD,
@@ -15,6 +27,8 @@ OPERATIONS = [
     MUL,
     DIV
 ]
+
+memory = 0
 
 
 def calculate(x, y, operation):
@@ -51,10 +65,39 @@ def check_number(x):
         x = int(x)
     elif is_float(x):
         x = float(x)
+    elif x == 'M':
+        x = memory
     else:
         raise ValueError(msg_1)
 
     return x
+
+
+def is_one_digit(num: float):
+    if -10 < num < 10:
+        if num.is_integer():
+            return True
+
+    return False
+
+
+def check(x, y, operator) -> None:
+    msg = ""
+
+    x = float(x)
+    y = float(y)
+
+    if is_one_digit(x) and is_one_digit(y):
+        msg += msg_6
+
+    if (x == 1 or y == 1) and operator == MUL:
+        msg += msg_7
+
+    if (x == 0 or y == 0) and operator in [MUL, SUB, ADD]:
+        msg += msg_8
+
+    if msg != "":
+        print(msg_9 + msg)
 
 
 def main():
@@ -63,13 +106,54 @@ def main():
     x = check_number(x)
     y = check_number(y)
 
-    print(float(calculate(x, y, operation)))
+    check(x, y, operation)
+
+    return float(calculate(x, y, operation))
+
+
+def need_memorize(res):
+    print(msg_4)
+    msg_index = 10
+
+    msgs = {
+        10: msg_10,
+        11: msg_11,
+        12: msg_12
+    }
+
+    memorize = input().strip()
+    if memorize == YES and is_one_digit(res):
+        while True:
+            print(msgs.get(msg_index))
+            answer = input().strip()
+            if answer == NO:
+                break
+            if answer == YES:
+                if msg_index < 12:
+                    msg_index += 1
+                    continue
+                return True
+
+        return False
+
+    return True
 
 
 while True:
     try:
         print(msg_0)
-        main()
+        result = main()
+        print(result)
+
+        if need_memorize(result):
+            memory = result
+
+        print(msg_5)
+        need_continue = input().strip()
+
+        if need_continue == YES:
+            continue
+
         break
     except ValueError as err:
         print(err)
--- Honest Calculator/task/tests.py
+++ Honest Calculator/task/tests.py
@@ -5,7 +5,15 @@ msg = ["Enter an equation",
        "Yes ... an interesting math operation. You've slept through all classes, haven't you?",
        "Yeah... division by zero. Smart move...",
        "Do you want to store the result? (y / n):",
-       "Do you want to continue calculations? (y / n):"]
+       "Do you want to continue calculations? (y / n):",
+       " ... lazy",
+       " ... very lazy",
+       " ... very, very lazy",
+       "You are",
+       "Are you sure? It is only one digit! (y / n)",
+       "Don't be silly! It's just one number! Add to the memory? (y / n)",
+       "Last chance! Do you really want to embarrass yourself? (y / n)"
+]
 
 
 def add_enter(txt):
@@ -16,13 +24,12 @@ def add_memory(txt):
     return "\n".join([txt, msg[4]])
 
 
-data = [
-            (("4 * 5", add_memory("20.0")), ("y", msg[5]), ("n", "")),
-            (("4 * 5.2", add_memory("20.8")), ("y", msg[5]), ("y", msg[0]),
-             ("1 + M", add_memory("21.8")), ("y", msg[5]), ("n", "")),
-            (("2 + 5", add_memory("7.0")), ("n", msg[5]), ("y", msg[0]),
-             ("21.0 / M", add_enter(msg[3])), ("5 + M", add_memory("5.0")),
-             ("y", msg[5]), ("n", "")),
+data = [(("4 * 5.0", "\n".join([msg[9] + msg[6], add_memory("20.0")])), ("n", msg[5]), ("n", "")),
+        (("2 + 5.5", add_memory("7.5")), ("y", msg[5]), ("y", msg[0]), ("M - 9", add_memory("-1.5")), ("n", msg[5]), ("n", "")),
+        (("225 / 15", add_memory("15.0")), ("y", msg[5]), ("y",msg[0]),
+         ("1 * 5", "\n".join([msg[9] + msg[6] + msg[7], add_memory("5.0")])), ("y", msg[10]), ("y", msg[11]), ("n", msg[5]), ("y", msg[0]),
+        ("M - 10", add_memory("5.0")), ("y", msg[10]), ("y", msg[11]), ("y", msg[12]), ("y", msg[5]), ("y", msg[0]),
+         ("M / M",  "\n".join([msg[9] + msg[6], add_memory("1.0")])), ("n", msg[5]), ("n", "")),
        ]  # (input data, msg sentence])
 
 
@@ -36,10 +43,9 @@ class HonestCalc(StageTest):
         for item in items:
             output = pr.execute(item[0])
             if item[1] != output.strip():
-                return CheckResult.wrong(f"Expected: ({item[1]});\nFound:    ({output.strip()})")
+                return CheckResult.wrong(f"Expected:\n{item[1]}\nFound:\n{output.strip()}")
         if not pr.is_finished():
             return CheckResult.wrong("Your program unnecessarily waiting for input.")
-
         return CheckResult.correct()
 
 
